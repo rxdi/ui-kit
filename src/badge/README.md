dc8f83bb4c544b2b17bfb2a13fb4e831dcfa9fd2
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2019-10-15 16:11:33 +0300

    feature(Badge): component initialization and README.md

---
+++ src/badge/README.md
@@ -0,0 +1,8 @@
+
+# Badge Component
+
+##### Basic Usage
+
+```html
+<rx-badge>1</rx-image>
+```
---
+++ src/badge/badge.component.ts
@@ -0,0 +1,12 @@
+import { Component, html, LitElement } from '@rxdi/lit-html';
+
+/**
+ * @customElement rx-badge
+ */
+@Component({
+  selector: 'rx-badge',
+  template(this: BadgeComponent) {
+    return html``;
+  }
+})
+export class BadgeComponent extends LitElement {}
