4fd52241dd5d09f890aa683a9eb483e799e89116
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2020-05-29 09:57:34 +0300

    feature(GlobalMenu): added menuOpened subject

--- src/global-menu/global-menu.service.ts
+++ src/global-menu/global-menu.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@rxdi/core';
 import { GlobalMenuComponent } from './global-menu.component';
 import './global-menu.component';
 import { TemplateResult } from '@rxdi/lit-html';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class GlobalMenuService {
   element: GlobalMenuComponent;
+  private menuOpened: Subject<boolean> = new Subject();
   open(template?: TemplateResult) {
     if (this.element) {
      this.close();
@@ -13,9 +15,11 @@ export class GlobalMenuService {
     this.element = document.createElement('global-menu') as GlobalMenuComponent;
     this.element.open(template);
     document.body.appendChild(this.element);
+    this.menuOpened.next(true);
   }
   close() {
     this.element.cancel();
     this.element.remove();
+    this.menuOpened.next(false);
   }
 }
