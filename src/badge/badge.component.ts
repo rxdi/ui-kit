97d5018f808449428166f68ad74b428849afbc97
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2020-11-01 10:18:25 +0200

    feat(Badge): added options to controll css attributes

--- src/badge/badge.component.ts
+++ src/badge/badge.component.ts
@@ -3,10 +3,18 @@ import {
   html,
   styleMap,
   LitElement,
-  property
+  property,
 } from '@rxdi/lit-html';
 import { PalettesUnion } from '../settings';
 
+export interface BadgeOptions {
+  boarderRadius: number;
+  height: number;
+  fontSize: number;
+  minWidth: number;
+  color: string;
+  background: string;
+}
 /**
  * @customElement rx-badge
  */
@@ -14,39 +22,54 @@ import { PalettesUnion } from '../settings';
   selector: 'rx-badge',
   template(this: BadgeComponent) {
     return html`
-    <style>
-    .badge {
-      box-sizing: border-box;
-      min-width: 22px;
-      height: 22px;
-      padding: 0 5px;
-      border-radius: 500px;
-      vertical-align: middle;
-      font-size: 14px;
-      display: inline-flex;
-      justify-content: center;
-      align-items: center;
-      color: var(--${this.palette}-color);
-      background: var(--${this.palette}-bg-color);
-    }
-    </style>
+      <style>
+        .badge {
+          box-sizing: border-box;
+          min-width: ${this.options.minWidth}px;
+          height: ${this.options.height}px;
+          padding: 0 5px;
+          border-radius: ${this.options?.boarderRadius}px;
+          vertical-align: middle;
+          font-size: ${this.options.fontSize}px;
+          display: inline-flex;
+          justify-content: center;
+          align-items: center;
+          color: var(--${this.palette}-color);
+          background: var(--${this.palette}-bg-color);
+        }
+      </style>
       <span
         style=${styleMap(
-          this.palette ? {} : { color: this.color, background: this.background }
+          this.palette ? {} : { color: this.color || this.options.color, background: this.background || this.options.background }
         )}
         class="badge"
         ><slot></slot
       ></span>
     `;
-  }
+  },
 })
 export class BadgeComponent extends LitElement {
   @property({ type: String })
-  public background = '#1e87f0';
+  public background: string;
 
   @property({ type: String })
-  public color = '#fff';
+  public color: string;
+
+  @property({ type: Object })
+  public options: Partial<BadgeOptions>;
 
   @property({ type: String })
   public palette: PalettesUnion;
+
+  OnUpdate() {
+    this.options = {
+      boarderRadius: 500,
+      height: 22,
+      fontSize: 14,
+      minWidth: 22,
+      color: '#FFF',
+      background: '#1e87f0',
+      ...this.options,
+    };
+  }
 }
