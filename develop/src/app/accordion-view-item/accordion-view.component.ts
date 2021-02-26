f0a238b7e4ec78cf2ac719df3279d7dcc9e25df4
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2021-02-26 17:51:51 +0200

    fix(accordion-item): view component modified

--- develop/src/app/accordion-view-item/accordion-view.component.ts
+++ develop/src/app/accordion-view-item/accordion-view.component.ts
@@ -1,19 +1,32 @@
-import { html, Component, LitElement } from '@rxdi/lit-html';
+import { html, Component, LitElement, css } from '@rxdi/lit-html';
 
 /**
  * @customElement accordion-view-component
  */
 @Component({
   selector: 'accordion-item-view-component',
+  style: css`
+  .container {
+    background: rgb(249, 250, 252);
+    padding: 50px;
+  }
+  `,
   template(this: AccordionItemViewComponent) {
     return html`
-      <rx-accordion-item @clicked=${(e) => console.log(e.detail)}>
+     <div class="container">
+     <rx-accordion-item @clicked=${(e) => console.log(e.detail)}>
         <div slot="title">Why should i upgrade ?</div>
         <div slot="content">Optimized for best experience...</div>
       </rx-accordion-item>
-      <markdown-reader
+      <rx-accordion-item @clicked=${(e) => console.log(e.detail)}>
+        <div slot="title">Is this the best choice ?</div>
+        <div slot="content">Probably not but it will do the job...</div>
+      </rx-accordion-item>
+    </div>
+    <markdown-reader
         link="https://raw.githubusercontent.com/rxdi/ui-kit/master/src/accordion-item/README.md"
       ></markdown-reader>
+
     `;
   },
 })
