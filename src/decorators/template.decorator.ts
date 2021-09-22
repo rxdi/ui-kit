764c54b5f394912fd71ab65c6221a7ffd9ea09e3
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2021-09-22 13:49:02 +0300

    fix(template): decorator fixed with never

--- src/decorators/template.decorator.ts
+++ src/decorators/template.decorator.ts
@@ -29,23 +29,21 @@ const regexes = [/\${[^{]+}/g, /\@\{(.*?)(?!\@\{)\}/g, /{(.*?)}/g];
 
 export function Template<T>(options: TemplateOptions<T>) {
   function interpolate(this: LitElement, string: any) {
-    return string.replace(regexes[0], match =>
+    return string.replace(regexes[0], (match) =>
       match
         .slice(2, -1)
         .trim()
         .split('.')
-        .reduce((el: LitElement, k: string) => String(el[k]) , this)
+        .reduce((el: LitElement, k: string) => String(el[k]), this)
     );
   }
-  return function(target: typeof LitElement) {
-    target.prototype['getTemplate'] = async function() {
+  return function (target: typeof LitElement) {
+    target.prototype['getTemplate'] = async function () {
       return unsafeHTML(interpolate.call(this, await options.templateUrl));
     };
-    target.prototype['render'] = function() {
-      return html`
-        ${async(this.getTemplate())}
-      `;
+    target.prototype['render'] = function () {
+      return html` ${async(this.getTemplate())} `;
     };
-    return Component(options)(target) as never;
+    return Component(options as never)(target) as never;
   };
 }
