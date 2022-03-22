22729ab4554cdbd50d9f616804da647ae264b85b
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2022-03-22 13:46:33 +0200

    fix(divider): added margin to icon class

--- src/divider/style.ts
+++ src/divider/style.ts
@@ -8,6 +8,7 @@ export const style = css`
     background-image: url(data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2220%22%20height%3D%2220%22%20viewBox%3D%220%200%2020%2020%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0A%20%20%20%20%3Ccircle%20fill%3D%22none%22%20stroke%3D%22%23e5e5e5%22%20stroke-width%3D%222%22%20cx%3D%2210%22%20cy%3D%2210%22%20r%3D%227%22%20%2F%3E%0A%3C%2Fsvg%3E%0A);
     background-repeat: no-repeat;
     background-position: 50% 50%;
+    margin: 20px 0 20px 0;
   }
   hr {
     border: 0;
